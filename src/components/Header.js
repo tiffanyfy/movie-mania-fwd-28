0f99d63c6191b0163f0bd0d7ea89d48914877467
tiffanyfy <tiffanyyiu3@gmail.com>
2021-08-17 11:18:34 -0700

    Add nav bar to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+import NavMain from './NavMain';
+
 function Header() {
     return (
         <header>
-            <h1>Movie Mania</h1>
+            <h1><Link to="/">Movie Mania</Link></h1>
+            <NavMain />
         </header>
     )
 }
