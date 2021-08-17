8ff0049be5811f49db36f0eb344262416ff7e6f6
tiffanyfy <tiffanyyiu3@gmail.com>
2021-08-17 10:37:05 -0700

    Create Header and Footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; FWD 28 Class</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header>
+            <h1>Movie Mania</h1>
+        </header>
+    )
+}
+
+export default Header;
