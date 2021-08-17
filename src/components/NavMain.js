182915c91c4f58c8656957f018e9be5731695fa7
tiffanyfy <tiffanyyiu3@gmail.com>
2021-08-17 11:12:29 -0700

    Create NavMain.js

---
+++ src/components/NavMain.js
@@ -0,0 +1,21 @@
+import { NavLink } from 'react-router-dom';
+
+function NavMain() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about">About</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/favs">Favs</NavLink>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavMain
