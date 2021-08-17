e0f900db5b0865a339612c2d3db2177fe5a82dc6
tiffanyfy <tiffanyyiu3@gmail.com>
2021-08-17 15:02:40 -0700

    Update PageHome.js

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -1,7 +1,35 @@
+import { useEffect } from 'react';
 import NavSort from '../components/NavSort';
 import Movies from '../components/Movies';
+import { API_TOKEN } from '../globals/globals';
 
-function PageHome() {
+function PageHome({sort}) {
+
+    useEffect(() => {
+
+        const fetchMovies = async () => {
+
+            // Call the API, waiting for response
+            // Get data for the whole “sort” feature, popular, top rated, etc
+            const res = await fetch(`https://api.themoviedb.org/3/movie/${sort}?&language=en-US&page=1`, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + API_TOKEN
+                }
+            });
+
+            // When we get the response, call the json method and assign to variable
+            let rawMovieData = await res.json();
+            rawMovieData = rawMovieData.results.splice(0, 12);
+            console.log(rawMovieData);
+
+        }
+
+        fetchMovies();
+
+    }, [sort]);
+
     return (
         <section className="home-page">
             <NavSort />
