179884d750d6d5ccf72557954b6466cf4d046a2f
tiffanyfy <tiffanyyiu3@gmail.com>
2021-08-17 11:08:01 -0700

    Add switch to AppRouter

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,8 +1,27 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import PageHome from '../pages/PageHome';
+import PageAbout from '../pages/PageAbout';
+import PageFavs from '../pages/PageFavs';
+import PageSingleMovie from '../pages/PageSingleMovie';
+
 function AppRouter() {
   return (
-    <div className="wrapper">
-      ... use app router instead
-    </div>
+    <Router>
+      <div className="wrapper">
+        <Header />
+          <main>
+            <Switch>
+              <Route path="/" exact><PageHome /></Route>
+              <Route path="/about"><PageAbout /></Route>
+              <Route path="/favs"><PageFavs /></Route>
+              <Route path="/movie/:id"><PageSingleMovie /></Route>
+            </Switch>
+          </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
