d79ac4cf4d0391cb28e255db148e1b82cdfda4f0
tiffanyfy <tiffanyyiu3@gmail.com>
2021-08-17 21:11:06 -0700

    Update PageSingleMovie.js

--- src/pages/PageSingleMovie.js
+++ src/pages/PageSingleMovie.js
@@ -1,6 +1,12 @@
 import SingleMovie from '../components/SingleMovie';
+import { useParams } from 'react-router-dom';
 
 function PageSingleMovie() {
+
+    const { id } = useParams();
+
+    console.log(id);
+
     return (
         <section className="single-movie-page">
             <SingleMovie />
